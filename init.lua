cfaec588b63b3508b13f0cb39406ca23515b3f4b
jlonic <josiplonic@hotmail.com>
2025-07-29 00:00:58 +0200

    transparent background

--- init.lua
+++ init.lua
@@ -15,3 +15,15 @@ local opts = {}
 
 require("vim-options")
 require("lazy").setup("plugins")
+
+vim.cmd [[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NormalNC guibg=NONE ctermbg=NONE
+  highlight EndOfBuffer guibg=NONE ctermbg=NONE
+  highlight SignColumn guibg=NONE ctermbg=NONE
+  highlight LineNr guibg=NONE ctermbg=NONE
+  highlight NeoTreeNormal guibg=NONE ctermbg=NONE
+  highlight NeoTreeNormalNC guibg=NONE ctermbg=NONE
+  highlight NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+  highlight NeoTreeWinSeparator guibg=NONE ctermbg=NONE
+]]
